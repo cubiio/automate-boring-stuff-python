03047e0022ee58cbe2096a1bf661a454c42bf616
Sam Atkins <sam@cubi.io>
2017-01-08 09:01:49 +0000

    feat: Add regex lesson

---
+++ reginald.py
@@ -0,0 +1,14 @@
+#! python3
+# playing around with regex
+
+import re
+
+
+namesRegex = re.compile(r'Agent \w+')
+print (namesRegex.sub(
+    'CENSORED', 'Agent Alice gave the secret documents to Agent Bob'))
+
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+print (agentNamesRegex.sub(r'\1****',
+                           'Agent Alice told Agent Carol that Agent Eve'
+                           'knew Agent Bob was a double agent'))
